13885f6826303ec027c06af28a59c1d0db1b76f0
Jevro1337 <malekamir56@gmail.com>
2023-10-12 21:01:41 +0100

    Optimized Code

--- main/main.ino
+++ main/main.ino
@@ -4,7 +4,7 @@
 #include <WiFiClientSecureBearSSL.h>
 #include <ESP8266WebServer.h>
 #include "RF433send.h"
-#include <Arduino_JSON.h>
+#include <ArduinoJson.h>
 #define PIN_RFOUT 3
 
 RfSend *tx_whatever;
@@ -54,11 +54,9 @@ void setup() {
   }
   Serial.println("");
   Serial.println("WiFi connected");
-   
-  // Define server routes
-  server.on("/", HTTP_GET, handleRoot);
 
-  // Start the server
+  server.on("/", HTTP_GET, handleRoot);
+  
   server.begin();
   Serial.println("Server started");
  
@@ -71,90 +69,77 @@ void setup() {
 }
 
 void handleRoot() {
-  std::unique_ptr<BearSSL::WiFiClientSecure>clientHTTPS(new BearSSL::WiFiClientSecure);
-
-    // Ignore SSL certificate validation
-    clientHTTPS->setInsecure();
-    
-    //create an HTTPClient instance
-    HTTPClient https;
-
-      if (https.begin(*clientHTTPS , "YOUR_API_URL")) {  // Use an API, or check readme for an alternative. Example response: {"code": "0x01 0x02 0x03 0x04 0x05 0x06 0x07"}
-      // start connection and send HTTP header
-      int httpCode = https.GET();
-      // httpCode will be negative on error
-      if (httpCode > 0) {
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-          String payload = https.getString();
-          Serial.println(payload);
-          JSONVar Data = JSON.parse(payload);
-          
-          String decoded = Data["code"];
-          byte data[7];
-          byte dataalt1[7];
-          byte dataalt2[7];
-          byte dataalt3[7];
-          int i = 0;
-          int j = 0;
-          int k= 0;
-          int l = 0;
-          char *ptr = strtok(strdup(decoded.c_str()), " ");
-          while (ptr != NULL) {
-             data[i++] = strtol(ptr, NULL, 16);
-              dataalt1[j++] = strtol(ptr, NULL, 16);
-             dataalt2[k++] = strtol(ptr, NULL, 16);
-             dataalt3[l++] = strtol(ptr, NULL, 16);
-             ptr = strtok(NULL, " ");
-          }
-
-          dataalt1[1] -= 0x30;
-          dataalt2[1] -= 0x20;
-          dataalt3[1] -= 0x50;
-  
-        Serial.println("===============DATA-SENT=============");
-              for (int i = 0; i < sizeof(data); i++) {
-                Serial.print(data[i], HEX);
-                Serial.print(" ");
-              }
-              Serial.println();
-              
-              for (int i = 0; i < sizeof(dataalt1); i++) {
-                Serial.print(dataalt1[i], HEX);
-                Serial.print(" ");
-              }
+ if (server.hasArg("code1") || server.hasArg("code2")) {
+    String payload = server.arg("code1");
+    Serial.println(payload);
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
       
-              Serial.println();
-              for (int i = 0; i < sizeof(dataalt2); i++) {
-                Serial.print(dataalt2[i], HEX);
-                Serial.print(" ");
-              }
-      
-              Serial.println();
-              for (int i = 0; i < sizeof(dataalt3); i++) {
-                Serial.print(dataalt3[i], HEX);
-                Serial.print(" ");
-              }
-      
-              Serial.println();
-              Serial.println("===============DATA-SENT=============");
-
-          //now we need to send the data and dataalt to the transmitter
-          tx_whatever->send(sizeof(data), data);
-          tx_whatever->send(sizeof(dataalt1), dataalt1);
-          tx_whatever->send(sizeof(dataalt2), dataalt2);
-          tx_whatever->send(sizeof(dataalt3), dataalt3);
-        }
-      } else {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-      }
-
-      https.end();
-    } else {
-      Serial.printf("[HTTPS] Unable to connect\n");
+      String decoded = doc["code"];
+    byte data[7];
+    byte dataalt1[7];
+    byte dataalt2[7];
+    byte dataalt3[7];
+    int i = 0;
+    int j = 0;
+    int k= 0;
+    int l = 0;
+    char *ptr = strtok(strdup(decoded.c_str()), " ");
+    while (ptr != NULL) {
+        data[i++] = strtol(ptr, NULL, 16);
+        dataalt1[j++] = strtol(ptr, NULL, 16);
+        dataalt2[k++] = strtol(ptr, NULL, 16);
+        dataalt3[l++] = strtol(ptr, NULL, 16);
+        ptr = strtok(NULL, " ");
     }
-    server.send(200, "text/html", "<!DOCTYPE HTML><html><body><h1 style='font-size: 10vw;margin-top:50%;text-align: center;display; block;'>Sent!<br><a href='javascript:window.location.reload(true)'>Request Again</a></h1></body></html>"); // Send a "Hello, World!" response
+  if (server.hasArg("code1")) {
+    dataalt1[1] -= 0x30;
+    dataalt2[1] -= 0x20;
+    dataalt3[1] -= 0x50;
+  }
+  if (server.hasArg("code2")) {
+      dataalt1[1] += 0x30;
+      dataalt2[1] += 0x20;
+      dataalt3[1] -= 0x32;
+  }
+
+  Serial.println("===============DATA-SENT=============");
+        for (int i = 0; i < sizeof(data); i++) {
+          Serial.print(data[i], HEX);
+          Serial.print(" ");
+        }
+        Serial.println();
+        
+        for (int i = 0; i < sizeof(dataalt1); i++) {
+          Serial.print(dataalt1[i], HEX);
+          Serial.print(" ");
+        }
+
+        Serial.println();
+        for (int i = 0; i < sizeof(dataalt2); i++) {
+          Serial.print(dataalt2[i], HEX);
+          Serial.print(" ");
+        }
+
+        Serial.println();
+        for (int i = 0; i < sizeof(dataalt3); i++) {
+          Serial.print(dataalt3[i], HEX);
+          Serial.print(" ");
+        }
+
+        Serial.println();
+        Serial.println("===============DATA-SENT=============");
+
+    //now we need to send the data and dataalt to the transmitter
+    tx_whatever->send(sizeof(data), data);
+    tx_whatever->send(sizeof(dataalt1), dataalt1);
+    tx_whatever->send(sizeof(dataalt2), dataalt2);
+    tx_whatever->send(sizeof(dataalt3), dataalt3);
+        
+    server.send(200, "text/plain", "recv");
+  }
 }
+
  
 void loop() {
   server.handleClient(); // Handle incoming client requests
